c14bf291d4b7c91ccce63c98d5e54572d7b71485
Jacky Cao <jackycao@outlook.com>
2023-05-08 16:09:55 +0300

    timers

--- client.py
+++ client.py
@@ -5,7 +5,12 @@ import struct
 
 import numpy as np
 
+import time
+
 def client_tcp():
+    client_results = input("Client results file name: ")
+    results_file = open(f'results/{client_results}.txt', "a")
+
     host = "10.38.151.146"
     port = 5000 
 
@@ -18,24 +23,41 @@ def client_tcp():
 
     count = 0
     success = 1
-  
+
     while success:
         # vidObj object calls read
         # function extract frames
         print("Loading frame")
+        load_start = int(time.time_ns()/1000)
         success, image = input_video.read()
+        load_end = int(time.time_ns()/1000)
+        results_file.write(f'Frame {count} loaded in {load_end-load_start} ms')
 
         print(f'Pickling Frame {count}')
+        pickle_start = int(time.time_ns()/1000)
         data = pickle.dumps(image, protocol=5)
+        pickle_end = int(time.time_ns()/1000)
+
         pickle_len = len(data)
         print(f'Pickled Frame {count} which has length {pickle_len} B')
+        results_file.write(f'Pickled Frame {count} which has length {pickle_len} B')
+        results_file.write(f'Frame {count} pickled in {pickle_end-pickle_start} ms')
 
         total_packets = int(np.ceil(pickle_len / 16384))
         print(f'Total number of packets to transmit is {total_packets}')
+        results_file.write(f'Total number of packets to transmit is {total_packets}')
 
         message_size = struct.pack("L", pickle_len)
 
+        send_start = int(time.time_ns()/1000)
         client_socket.send(message_size + data)  # send message
+        send_end = int(time.time_ns()/1000)
+        results_file.write(f'Frame {count} sent in {send_end-send_start} ms')
+
+        recv = client_socket.recv(1024).decode()  # receive response
+        response_start = int(time.time_ns()/1000)
+
+        results_file.write(f'Frame {count} response received in {response_start-send_end} ms')
 
         print(count, pickle_len)
         # Saves the frames with frame-count
