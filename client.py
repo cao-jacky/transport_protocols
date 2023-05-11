ff8cb611036f5507096a9cab5bd11543a473ac44
Jacky Cao <jackycao@outlook.com>
2023-05-11 13:33:31 +0300

    server details for udp

--- client.py
+++ client.py
@@ -149,7 +149,7 @@ def client_udp(video_file, results_file_name):
             # data_to_send = bytearray(payload_size)
 
             data_to_send = curr_packet_bytes + total_pakcets_bytes + img_part
-            sock.sendto(data_to_send)
+            sock.sendto(data_to_send, server_addr)
             packet_end = int(time.time_ns()/1000)
             results_file.write(f'[{time.time_ns()/1000}] Frame {count} sent packet {curr_packet} of {total_packets} in {packet_end-packet_start} ms\n')
             
